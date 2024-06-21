84519344f784b12dd0bf9cd0bb04a52dc9869d53
Cris <go.cristopher@gmail.com>
2024-06-21 16:05:45 +0800

    Created search algorithm in search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,47 @@ def search_users(args):
     """
 
     # Implement search here!
+    if not args:
+        return USERS
+    
+    id_match = []
+    name_match = []
+    age_match = []
+    occupation_match = []
+    
 
-    return USERS
+    for user in USERS:
+        max_age = None
+        min_age = None
+
+        if "age" in args:
+            max_age = int(args["age"]) + 1
+            min_age = int(args["age"]) - 1
+        
+        if "id" in args:
+            if len(id_match) == 0:
+                if int(user["id"]) == int(args["id"]):
+                    id_match.append(user)
+                    continue
+
+        if "name" in args:
+            if args["name"].lower() in user["name"].lower():
+                name_match.append(user)
+
+        if "age" in args:
+            if (int(user["age"]) >= min_age and int(user["age"]) <= max_age):
+                age_match.append(user)
+
+        if "occupation" in args:
+            if args["occupation"] in user["occupation"]:
+                occupation_match.append(user)
+
+    all_matches = id_match + name_match + age_match + occupation_match
+    result = []
+
+    # clean duplicates 
+    for item in all_matches:
+        if item not in result:
+            result.append(item)
+
+    return result
